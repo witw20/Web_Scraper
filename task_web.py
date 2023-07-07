ebdb6c765a5aba148b94315a34ff67e183fb72a0
witw20 <64714263+witw20@users.noreply.github.com>
2023-07-07 09:13:55 +0800

    Add files via upload

--- task_web.py
+++ task_web.py
@@ -1,13 +1,34 @@
 import requests
 
+from bs4 import BeautifulSoup
+
 
 def main():
     url = input("Input the URL:\n")
+    # get_jason(url)
+    get_html(url)
+
+def get_jason(url):
     r = requests.get(url)
     if r:
         print(r.json()['content'])
     else:
         print("Invalid quote resource!")
+
+def get_html(url):
+    if url.startswith('https://www.nature.com') and \
+        'article' in url:
+        info = dict()
+        r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+        soup = BeautifulSoup(r.content, 'html.parser')
+        title = soup.find('title')
+        info["title"] = title.text
+        description = soup.find('meta', {'name': 'description'})
+        info["description"] = description.text
+        print(info)
+    else:
+        print('Invalid page!')
+
 
 if __name__ == '__main__':
     main()
